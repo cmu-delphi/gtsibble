e73fcec231bd7e6ff2b520df107340e5157b33fc
Quang Nguyen <quang.p.nguyen.gr@dartmouth.edu>
2023-01-18 19:30:33 -0500

    Re-defined initialize prototype to have delphi-utils loader only

--- R/loader.R
+++ R/loader.R
@@ -1,14 +1,12 @@
 # On-load and installation support functionality
-# Updated: 2022-04-09
+# Updated: 2023-01-18
 # Ref: https://rstudio.github.io/reticulate/articles/package.html
 
 # Global reference to delphi_utils
 delphi_utils <- NULL
 
 .onLoad <- function(libname, pkgname){
-    reticulate::configure_environment(pkgname)
     delphi_utils <<- reticulate::import("delphi_utils", delay_load = TRUE)
-    message("Using delphi-utils version ", delphi_utils[["__version__"]])
 }
 
 #' @title Installing \code{delphi-utils} using \code{reticulate}
@@ -17,23 +15,45 @@ delphi_utils <- NULL
 #'     available on Windows.
 #' @param conda The path to conda executable. By default, "auto" will allow
 #'     \code{reticulate} to automatically find an appropriate conda-binary
+#' @param force (logical). Whether to force install \code{delphi-utils} if package
+#'     is already installed.
 #' @param ... Additional arguments passed to [reticulate::py_install]. Since
 #'     \code{delphi-utils} is only available on PyPi, pip is always set to TRUE.
 #' @seealso [py_install]
 #' @export
-load_delphi_utils <- function(method = "auto", conda = "auto", ...){
+install_delphi_utils <- function(method = "auto", conda = "auto", force = FALSE, ...){
     if (reticulate::py_module_available("delphi_utils")){
-        delphi_utils <<- reticulate::import("delphi_utils", delay_load = TRUE)
-        message("Using delphi-utils version ", delphi_utils[["__version__"]])
-    } else {
-        if (grepl(pattern = "Windows", osVersion)){
+        message("delphi-utils already installed in this environment")
+    }
+
+    if (reticulate::py_module_available("delphi_utils") == FALSE | force == TRUE){
+        if (grepl(pattern = "Windows", utils::osVersion)){
             message("Installing geopandas for Windows...")
-            reticulate::py_install("geopandas", method = method, conda = conda,
+            reticulate::py_install("geopandas", method = method,
+                                   conda = conda,
                                    pip = FALSE,
                                    ...)
         }
         message("installing delphi-utils")
         reticulate::py_install("delphi-utils", method = method, conda = conda,
                                pip = TRUE, ...)
+        message("Installation Complete")
+    }
+}
+
+#' @title Checking proper loading of \code{delphi-utils}
+#' @details This function double checks whether \code{delphi-utils} is available.
+#'     If it is available, returns the version installed.
+#' @return None
+#' @export
+check_delphi_utils <- function(){
+    if (!reticulate::py_module_available("delphi_utils")){
+        stop("delphi-utils not available. Please double check python environment or re-install delphi-utils")
+    } else {
+        if (!exists("delphi_utils")){
+            delphi_utils <<- reticulate::import("delphi_utils")
+        } else {
+            message(paste0("Using delphi-utils version ", delphi_utils[["__version__"]]))
+        }
     }
 }
